79453110197dcb90768caaaa464d4b5bdf0cb37e
praghu-b <prakashbalan555@gmail.com>
2026-04-17 14:31:35 +0530

    fix: update base URI for API connection

--- loop_tag/lib/app/utils/core/constants.dart
+++ loop_tag/lib/app/utils/core/constants.dart
@@ -2,7 +2,7 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 
 const String baseURI = String.fromEnvironment(
   'API_BASE_URL',
-  defaultValue: 'https://loop-tag.onrender.com/api',
+  defaultValue: 'https://loop-tag-om2z.onrender.com/api',
 );
 
 Future<bool> isConnected() async {
